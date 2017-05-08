f534644bdfbbc14c68f416806e26a212720a2295
llafont <lionel.lft@me.com>
2017-05-08 12:46:28 +0200

    Initial commit

---
+++ wp-content/themes/blogmpaw/banner.php
@@ -0,0 +1,21 @@
+<header>
+        <div class="logo">
+          <a href="http://www.mpaw.xyz"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo.svg" width="100%"/></a>
+        </div>
+        <div class="socials">
+          <div class="social_icon">
+            <a href="https://www.facebook.com/MaPetiteAgenceWeb/" target="blank"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logoFB.svg" width="50%" alt="Suivez-nous sur facebook"/></a>
+          </div>
+        </div>
+        <div class="motto">
+          <div class="motto_inner">
+            <h1>
+              L'agence web dont vous êtes le
+            </h1>
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/hero.svg" width="70%" alt="Image héro" alt="L'agence web dont vous êtes le héro"/>
+          </div>
+        </div>
+        <div class="tagline">
+          Communauté apprenante de la communication digitale
+        </div>
+      </header>
